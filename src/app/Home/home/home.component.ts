ad0681609057b5ae6e84b0529d1fede0f7241587
pranali phadatare <pranaliphadatare496@gmail.com>
2024-03-19 20:51:49 +0530

    we are live

---
+++ src/app/Home/home/home.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/Service/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+ myForm:any;
+  objects: any[]=[];
+  count: number = 13;
+  newObject: any = { id: '', name: '', data: { price: null, color: '' } };
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.fetchObjects();
+  }
+
+  fetchObjects(): void {
+    this.apiService.getObjects().subscribe(
+      (response: any[]) => {
+        this.objects = response;
+      },
+      (error) => {
+        console.error('Error fetching objects:', error);
+      }
+    );
+  }
+
+  addObject(): void {
+    this.newObject.id = this.count++;
+
+    this.apiService.addObject(this.newObject).subscribe(
+      (response: any) => {
+        console.log('Object added successfully:', response);
+        this.objects.push(this.newObject)
+        //this.newObject = { id: '', name: '', data: { price: null, color: '' } };
+      },
+      (error) => {
+        console.error('Error adding object:', error);
+      }
+    );
+
+   
+  }
+  del(index:number){
+    alert("Are you sure for delete the record")
+    this.objects.splice(index,1);
+  }
+  generateUniqueId(){
+    const timestamp = new Date().getTime();
+    return  this.count++;
+  }
+  clearForm(): void {
+    this.newObject = { name: '', data: { color: '' } };
+  }
+
+}
